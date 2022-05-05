31ffdb73865c7c9c594cac74695e76d3d1f68d9e
eliasGarciarena <102766956+eliasGarciarena@users.noreply.github.com>
2022-05-05 20:55:07 -0300

    Push #9: boton rancio

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,14 @@
-alert("Script cargado correctamente");
+function slider(){
+    let obj = document.getElementById("hidden");
+    if(estaVisible(obj)){
+        obj.style.display = "none";
+        console.log("asd");
+    } else{
+        obj.style.display = "block";
+        console.log("asdasds");
+    }
+}
+
+function estaVisible(obj) {
+    return obj.offsetWidth > 0 && obj.offsetHeight > 0;
+}
