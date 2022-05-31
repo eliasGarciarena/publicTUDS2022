2e0f10e3779e63cb2ce7bd1cf71316870efaffa4
eliasGarciarena <102766956+eliasGarciarena@users.noreply.github.com>
2022-05-31 17:01:11 -0300

    Push #11: carrusel v2

--- scripts/js.js
+++ scripts/js.js
@@ -1,8 +1,14 @@
-let carCont = 0, cantidad = 2;
+let carCont = 0, cantidad = 3;
+
+function estaVisible(obj){
+    return obj.offsetWidth > 0 && obj.offsetHeight > 0;
+}
 
 function slider(){
     let obj = document.getElementById("vista");
-
+    if(!estaVisible(obj)){
+        obj.style.display = "block";
+    }
     if(carCont == cantidad-1){
         carCont = 0;
         obj.src = "assets/0.png";
