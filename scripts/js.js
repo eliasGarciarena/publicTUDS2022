5e05cffc40d0d143f5e9ff6470b7294925fce5f9
eliasGarciarena <102766956+eliasGarciarena@users.noreply.github.com>
2022-05-31 19:50:38 -0300

    Push #12: form & stuff

--- scripts/js.js
+++ scripts/js.js
@@ -1,19 +1,23 @@
 let carCont = 0, cantidad = 3;
 
+window.onload = function getMenu(){
+    document.getElementById("menu").innerHTML = "    <div class='logo'>    <h1 style='text-align: center; width: 300px;'>Título Pru<span style='color:white;'>eba</span><br><span style='text-decoration:overline;'>Programación</span><span style='color:white; text-decoration: overline;'>&nbsp;Web I&nbsp;</span></h1>    </div>";
+}
+
 function estaVisible(obj){
     return obj.offsetWidth > 0 && obj.offsetHeight > 0;
 }
 
 function slider(){
-    let obj = document.getElementById("vista");
-    if(!estaVisible(obj)){
-        obj.style.display = "block";
+    let vista = document.querySelector("#vista");
+    if(!estaVisible(vista)){
+        vista.style.display = "block";
     }
     if(carCont == cantidad-1){
         carCont = 0;
-        obj.src = "assets/0.png";
+        vista.src = "assets/0.png";
     } else{
         carCont++;
-        obj.src = "assets/" + carCont + ".png";
+        vista.src = "assets/" + carCont + ".png";
     }
 }
