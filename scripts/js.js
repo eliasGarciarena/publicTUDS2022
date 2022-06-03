0e6641e3ad61dd4bf3ca060b1137468172e630df
eliasGarciarena <102766956+eliasGarciarena@users.noreply.github.com>
2022-06-02 21:01:16 -0300

    Push #15: validate.form()

--- scripts/js.js
+++ scripts/js.js
@@ -1,7 +1,7 @@
-let carCont = 0, cantidad = 3;
+let carCont = 0, cantidad = 3, errors;
 
 window.onload = function getMenu(){
-    document.getElementById("menu").innerHTML = "    <div class='logo'>    <h1 style='text-align: center; width: 300px;'>Título Pru<span style='color:white;'>eba</span><br><span style='text-decoration:overline;'>Programación</span><span style='color:white; text-decoration: overline;'>&nbsp;Web I&nbsp;</span></h1>    </div>";
+    document.getElementById("menu").innerHTML = "    <div class='logo' style='color:green;'>    <h1 style='text-align: center; width: 300px;'>Título Pru<span style='color:white;'>eba</span><br><span style='text-decoration:overline;'>Programación</span><span style='color:white; text-decoration: overline;'>&nbsp;Web I&nbsp;</span></h1>    </div>";
 }
 
 function estaVisible(obj){
@@ -19,5 +19,43 @@ function slider(){
     } else{
         carCont++;
         vista.src = "assets/" + carCont + ".png";
+    }
+}
+
+function validar(event){
+    errors = 0;
+    event.preventDefault();
+    vNombre();
+    vEdad();
+    let stringBuilder = "validar() throws " + errors + " errors :(";
+    if(errors!==0){
+        console.log(stringBuilder);
+    }
+    return false;
+}
+
+function vNombre(){
+    let obj = document.getElementById("f_nombre");
+
+    if(obj.value==""){
+        errors++;
+        alert("NOMBRE no puede estar vacío!");
+    }
+}
+
+function vEdad(){
+    let obj = document.getElementById("f_edad");
+
+    if(obj.value==""){
+        errors++;
+        alert("EDAD no puede estar vacío!");
+    } else if (!isNaN(obj.value)){
+        if(obj.value<=0){
+            errors++;
+            alert("EDAD inválida!");
+        }
+    } else{
+        errors++;
+        alert("EDAD debe ser un número!");
     }
 }
